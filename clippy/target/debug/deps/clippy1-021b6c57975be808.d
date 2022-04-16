9af001ee4aa0d317099008ed22354bf07efdb2b1
staszewski <kamilstaszewski95@gmail.com>
2022-04-16 15:57:48 +0200

    Start rustlings in 2022

---
+++ clippy/target/debug/deps/clippy1-021b6c57975be808.d
@@ -0,0 +1,7 @@
+/Users/kamilstaszewski/Projekty/rust/rustlings/exercises/clippy/target/debug/deps/clippy1-021b6c57975be808.rmeta: clippy1.rs
+
+/Users/kamilstaszewski/Projekty/rust/rustlings/exercises/clippy/target/debug/deps/clippy1-021b6c57975be808.d: clippy1.rs
+
+clippy1.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
